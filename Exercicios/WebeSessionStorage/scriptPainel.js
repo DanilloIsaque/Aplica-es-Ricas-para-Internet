72b39f386928931c84d2aea5b63be41585f4d8b2
Danillo Isaque Brito Lima <104601158+DanilloIsaque@users.noreply.github.com>
2023-03-22 22:05:10 -0300

    termino do painel e video

--- Exercicios/WebeSessionStorage/scriptPainel.js
+++ Exercicios/WebeSessionStorage/scriptPainel.js
@@ -2,6 +2,21 @@
 //e uma cor.
 var corfundo,cortexto,nome;
 
+function inicia(){
+    if(localStorage.nome)// se for verdadeiro, já tenho coisa no local storage
+    {
+        corfundo=localStorage.corfundo;
+        cortexto=localStorage.cortexto;
+        nome=localStorage.nome;
+        //chamamos a funcao defineCor(op,cor)
+        defineCor(1,corfundo);
+        defineCor(2,cortexto);
+        document.getElementById("painel").innerHTML="bem vindo, " + nome+ "<hr>";
+    
+    }
+}
+
+
 function defineCor(op,cor){
     //se op=1 ,altere a cor do fundo, se igual a 2, muda a do texto
     if(op==1){
@@ -15,9 +30,19 @@ function defineCor(op,cor){
     }
 }
 function gravar(){
+    var form = document.getElementById("fconf");
     nome= document.getElementById("fnome").value;
     //armazenar as informações no localStorage
     localStorage.corfundo=corfundo;
     localStorage.cortexto=cortexto;
     localStorage.nome=nome;
+    form.style.display="none";
+
 }
+
+function mostrarOpcoes(){
+    var form = document.getElementById("fconf");
+    form.style.display= "block";
+}
+
+window.addEventListener("load",inicia);//chama a função dps da pagina ter sido carregada
